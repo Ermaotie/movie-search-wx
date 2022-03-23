b41724f585c58cbbf824e2ee026d97b93b27904d
Ermaotie <63703577+Ermaotie@users.noreply.github.com>
2022-03-23 20:40:20 +0800

    Update main.py
    更新subscribe

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def hello(message):
     return yun_pan_pan(message.content)
 
 
+@robot.subscribe
+def subscribe():
+    return "感谢您的关注，发送关键词即可获取资源链接！"
+
+
 # 让服务器监听在 0.0.0.0:8080
 robot.config['HOST'] = '0.0.0.0'
 robot.config['PORT'] = 8080
